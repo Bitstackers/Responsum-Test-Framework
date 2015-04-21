6d664687f74e94e845b3ef5afed792bf3a555f31
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-04-21 22:11:04 +0200

    #3 #4 Simple test for picking company

--- src/main/java/pom/Home.java
+++ src/main/java/pom/Home.java
@@ -1,18 +1,64 @@
-package pom;
+package main.java.pom;
+
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
-/**
- * Created by Anna on 2015-04-13.
- */
 public class Home {
 
-    private static WebElement element = null;
+	private static WebElement element = null;
+	private static Select dropdown = null;
 
-    public static WebElement btn_Confirm(WebDriver driver) {
-        element = driver.findElement(By.id("confirm"));
-        return element;
-    }
+	public static WebElement btn_Save(WebDriver driver) {
+		element = driver.findElement(By.className("save pure-button"));
+		return element;
+	}
+
+	public static WebElement btn_Send(WebDriver driver) {
+		element = driver.findElement(By.className("send pure-button"));
+		return element;
+	}
+
+	public static WebElement btn_Cancel(WebDriver driver) {
+		element = driver.findElement(By.className("cancel pure-button"));
+		return element;
+	}
+
+	public static WebElement ui_contacts(WebDriver driver) {
+		element = driver.findElement(By.id("contactlist"));
+		return element;
+	}
+
+	public static WebElement spn_fd_Welcome(WebDriver driver) {
+		element = driver.findElement(By.id("welcome-message-text"));
+		return element;
+	}
+
+	public static WebElement spn_Company(WebDriver driver) {
+		element = driver.findElement(By
+				.xpath("//*[@id=\"companyselector\"]/div/a/span"));
+		return element;
+	}
+
+	public static List<WebElement> opts_Company(WebDriver driver) {
+		List<WebElement> list = driver.findElements(By
+				.xpath("//*[@id=\"companyselector\"]/div/div/ul/li"));
+		return list;
+	}
+
+	public static WebElement opt_Company(WebDriver driver, String text) {
+		element = driver.findElement(By
+				.xpath("//*[@id=\"companyselector\"]/div/div/ul/li[text()=\""
+						+ text + "\"]"));
+		return element;
+	}
+
+	public static WebElement txt_fd_Search(WebDriver driver) {
+		element = driver.findElement(By.id("contact-info-searchbar"));
+		return element;
+	}
+
 }
--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import main.java.pom.Helpers;
+import main.java.pom.Home;
 import main.java.pom.Login;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -22,6 +25,8 @@ public class SimpleTest {
 
 	WebDriver driver;
 	String password;
+	private static String LOGIN = "walach.anna.or";
+	private static boolean FIRST_TIME = false;
 
 	@BeforeTest
 	public void prepare() throws IOException {
@@ -35,31 +40,80 @@ public class SimpleTest {
 	}
 
 	@Test
-	public void firstTest() throws InterruptedException {
+	public void noOnesCalling() {
 		driver = new ChromeDriver();
 		driver.get("http://client.openreception.org");
+		System.out.println("Noone's calling");
 		System.out.println("Page Title is " + driver.getTitle());
 
 		// logging
-		Helpers.wait(driver);
-		Login.txt_fd_login(driver).sendKeys("walach.anna.or");
-		Login.txt_fd_password(driver).sendKeys(password);
-		Login.btn_signIn(driver).click();
-
-		Assert.assertEquals(true, true);
+		login();
+		checkElementsBeforeCall();
 		driver.quit();
 	}
 
-	// @Test
-	public void secondTest() {
+	@Test
+	public void companyPicked() {
+
 		driver = new ChromeDriver();
 		driver.get("http://client.openreception.org");
-		System.out.println("Page Title is " + driver.getCurrentUrl());
-		Assert.assertEquals("Google", driver.getTitle());
+		System.out.println("Company picked");
+		System.out.println("Page Title is " + driver.getTitle());
+
+		// logging
+		login();
+		selectingCompany("BitStackers");
 		driver.quit();
 	}
 
 	@AfterTest
 	public void closing() {
+	}
+
+	private void login() {
+		Helpers.wait(driver);
+		Login.txt_fd_login(driver).sendKeys(LOGIN);
+		Login.txt_fd_password(driver).sendKeys(password);
+		Login.btn_signIn(driver).click();
+		if (FIRST_TIME) {
+			Login.btn_accept(driver).click();
+		}
+	}
+
+	private void checkElementsBeforeCall() {
+		Helpers.wait(driver);
+		String welcome = Home.spn_fd_Welcome(driver).getText();
+		System.out.println("Welcome text is: " + welcome);
+		Assert.assertEquals(welcome, "Ikke i kald");
+		// java.lang.AssertionError: expected [] but found [Ikke i kald]
+		Helpers.wait(driver);
+		String company = Home.spn_Company(driver).getText();
+		Assert.assertEquals(company.length(),
+				"S?g efter en virksomhed".length());
+		Helpers.wait(driver);
+		System.out.println("Company name is: " + company);
+	}
+
+	private void selectingCompany(String companyName) {
+		Helpers.wait(driver);
+		Home.spn_Company(driver).click();
+		List<WebElement> options = Home.opts_Company(driver);
+		System.out.println(options.size());
+		for (WebElement opt : options) {
+			System.out.println(opt.getText() + " : " + opt.getText().length());
+			if (opt.getText().length() == companyName.length()) {
+				opt.click();
+				break;
+			}
+		}
+
+		String company = Home.spn_Company(driver).getText();
+		System.out.println("Company name is: " + company);
+		Assert.assertEquals(company, companyName);
+		Helpers.wait(driver);
+		String welcome = Home.spn_fd_Welcome(driver).getText();
+		System.out.println("Welcome text is: " + welcome);
+		Assert.assertEquals(welcome,
+				"Velkommen til BitStackers, hvad kan jeg hjælpe med?");
 	}
 }
