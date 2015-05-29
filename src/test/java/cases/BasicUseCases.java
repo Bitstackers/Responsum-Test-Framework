fbcbf26d367dbbeb612f9584a03cea130d39963b
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-05-29 16:35:02 +0200

    New "keyword" use case resolves #18

--- src/test/java/cases/BasicUseCases.java
+++ src/test/java/cases/BasicUseCases.java
@@ -36,7 +36,7 @@ public class BasicUseCases {
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("http://client.openreception.org");
-		System.out.println("***First use case***");
+		System.out.println("***Basic use case***");
 
 		Common.login(driver, LOGIN, password, false);
 		ShortcutsView.getReady(driver);
@@ -74,6 +74,60 @@ public class BasicUseCases {
 		Helpers.waiting(2000);
 
 		System.out.println(Constants.DEFAULT_EMPLOYEE_1 + " wants to talk");
+		ShortcutsView.transfer(driver);
+
+		System.out.println("Transfer completed, conversation's done");
+		Helpers.waiting(2000);
+
+		ShortcutsView.hangup(driver);
+		Common.hangOutEmployee();
+
+	}
+
+	@Test
+	public void should_call_specific_employee_by_keywords() {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://client.openreception.org");
+		System.out.println("***Basic use case by keywords***");
+
+		Common.login(driver, LOGIN, password, false);
+		ShortcutsView.getReady(driver);
+
+		System.out.println("Customer calls company: "
+				+ Constants.DEFAULT_COMPANY);
+		Common.callCompany(Constants.DEFAULT_COMPANY);
+
+		System.out.println("Receptionist pick up the call.");
+		ShortcutsView.pickup(driver);
+
+		Helpers.waiting(2000);
+
+		System.out.println("Customer asks for someone with knowledge about: "
+				+ Constants.DEFAULT_KEYWORD);
+
+		ShortcutsView.switchToSearch(driver);
+		Common.sendToLastActiveElement(Constants.DEFAULT_KEYWORD, driver);
+
+		System.out.println("Receptionist checks, if employee is available.");
+
+		ShortcutsView.switchToContactCalendar(driver);
+		HomeView.checkCalendar(Constants.CALENDAR_ENTRIES_BS, driver);
+
+		ShortcutsView.switchToCompanyCalendar(driver);
+		HomeView.checkContactEvents(Constants.EVENTS_ENTRIES_THOMAS, driver);
+
+		System.out.println("Receptionist informs, that employee is available.");
+
+		System.out.println("Receptionist calls: "
+				+ Constants.DEFAULT_EMPLOYEE_2);
+		HomeView.callSelectedPerson(driver);
+
+		System.out.println(Constants.DEFAULT_EMPLOYEE_2 + " answers");
+
+		Helpers.waiting(2000);
+
+		System.out.println(Constants.DEFAULT_EMPLOYEE_2 + " wants to talk");
 		ShortcutsView.transfer(driver);
 
 		System.out.println("Transfer completed, conversation's done");
--- src/test/java/helpers/Constants.java
+++ src/test/java/helpers/Constants.java
@@ -5,16 +5,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Constants {
-	public static final String EMPLOYEE_1_NR = null;
 	public static final String DEFAULT_ADDRESS_CUSTOMER = "http://192.168.2.192/";
 	public static final String DEFAULT_ADDRESS_RECEPTIONIST = "http://192.168.2.190/";
 	public static final String DEFAULT_ADDRESS_EMPLOYEE = "http://192.168.2.196/";
-	public static final String DEFAULT_EMPLOYEE_2 = "Thomas";
-	public static String DEFAULT_COMPANY = "BitStackers";
-	public static String DEFAULT_EMPLOYEE_1 = "Kim Rostgaard Christensen";
-	public static int CALENDAR_ENTRIES_BS = 2;
-	public static int EVENTS_ENTRIES_KIM = 0;
-	public static int EVENTS_ENTRIES_THOMAS = 4;
+	public static final String DEFAULT_COMPANY = "BitStackers";
+	public static final String DEFAULT_EMPLOYEE_1 = "Kim Rostgaard Christensen";
+	public static final String DEFAULT_EMPLOYEE_2 = "Thomas Løcke";
+	public static final String DEFAULT_KEYWORD = "yolk";
+
+	public static final int CALENDAR_ENTRIES_BS = 2;
+	public static final int EVENTS_ENTRIES_KIM = 0;
+	public static final int EVENTS_ENTRIES_THOMAS = 4;
 	public static Map<String, String> NUMBERS;
 	static {
 		Map<String, String> aMap = new HashMap<>();
